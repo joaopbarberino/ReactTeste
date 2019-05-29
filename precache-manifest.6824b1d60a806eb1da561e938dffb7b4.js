339835ae9716b0a615cd2cf18bb258c55dc89be8
João Pedro Barberino Silva <joao.p.barberino@gmail.com>
2019-05-29 10:50:12 -0300

    Updates

---
+++ precache-manifest.6824b1d60a806eb1da561e938dffb7b4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3dd786c7d446d52cc529045cb1a1092f",
+    "url": "/ReactTeste/index.html"
+  },
+  {
+    "revision": "a8ce83efe78a793ec65f",
+    "url": "/ReactTeste/static/css/main.7eb46015.chunk.css"
+  },
+  {
+    "revision": "607320d00d64b68e18da",
+    "url": "/ReactTeste/static/js/2.2050718e.chunk.js"
+  },
+  {
+    "revision": "a8ce83efe78a793ec65f",
+    "url": "/ReactTeste/static/js/main.819f2bc3.chunk.js"
+  },
+  {
+    "revision": "98ea14a5d52ee533ae39",
+    "url": "/ReactTeste/static/js/runtime~main.ab15ee01.js"
+  }
+]);
